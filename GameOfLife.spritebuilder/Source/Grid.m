5f33a90b07e8cb2b13715359b7004cb8483f98b7
Nathan Taylor <nathan@nathantaylor.com>
2014-07-08 01:40:17 -0700

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -150,6 +150,8 @@ static const int GRID_COLUMNS = 10;
 
 - (void)updateCreatures
 {
+    int numAlive = 0;
+    
     // iterate through the rows
     // note that NSArray has a method 'count' that will return the number of elements in the array
     for (int i = 0; i < [_gridArray count]; i++)
@@ -166,9 +168,14 @@ static const int GRID_COLUMNS = 10;
             else if (currentCreature.isAlive && (currentCreature.livingNeighbors < 2 || currentCreature.livingNeighbors > 3)) {
                 currentCreature.isAlive = NO;
             }
+            
+            if (currentCreature.isAlive) {
+                numAlive++;
+            }
         }
     }
-
+    
+    _totalAlive = numAlive;
 }
 
 @end
